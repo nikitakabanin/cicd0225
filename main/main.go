9eef3ebf779755ee0f3628cee6aa6974333b6296
Nikita Kabanin <n.kabanin@yandex.ru>
2026-02-14 00:46:50 +0300

    init commit

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+
+	fmt.Scan(&n)
+
+	fmt.Println(factorial(n))
+}
+
+func factorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
